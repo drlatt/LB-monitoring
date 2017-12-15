3f194ccc1ac2b688b612116d4ac7d41882f36d38
lateef <lateef.koshemani@konga.com>
2017-12-15 15:59:34 +0100

    added two more servers to instance.tf file

--- instance.tf
+++ instance.tf
@@ -1,6 +1,7 @@
 # instance properties
 
-resource "aws_instance" "test1" {
+#monitoring server and loadbalancer
+resource "aws_instance" "LB" {
   ami           = "${lookup(var.amis, var.aws_region)}"
   instance_type = "t2.micro"
   subnet_id     = "${var.aws_subnet_id}"
@@ -18,4 +19,28 @@ resource "aws_eip" "ip" {
 # output variables such as IP address
 output "ip" {
   value = "${aws_eip.ip.public_ip}"
+}
+
+#server1
+resource "aws_instance" "server1" {
+  ami           = "${lookup(var.amis, var.aws_region)}"
+  instance_type = "t2.micro"
+  subnet_id     = "${var.aws_subnet_id}"
+
+  # use the local-exec provisioner to run commands locally
+  /*  provisioner "local-exec" {
+    command = "echo ${aws_instance.test1.public_ip} > ip_address.txt"
+  } */
+}
+
+#server2
+resource "aws_instance" "server2" {
+  ami           = "${lookup(var.amis, var.aws_region)}"
+  instance_type = "t2.micro"
+  subnet_id     = "${var.aws_subnet_id}"
+
+  # use the local-exec provisioner to run commands locally
+  /*  provisioner "local-exec" {
+    command = "echo ${aws_instance.test1.public_ip} > ip_address.txt"
+  } */
 }
