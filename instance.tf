5bcfd305cf9c721e2f9597889d176e67db8ae291
lateef <lateef.koshemani@konga.com>
2017-12-15 16:24:25 +0100

    removed output files not needed

--- instance.tf
+++ instance.tf
@@ -5,20 +5,6 @@ resource "aws_instance" "LB" {
   ami           = "${lookup(var.amis, var.aws_region)}"
   instance_type = "t2.micro"
   subnet_id     = "${var.aws_subnet_id}"
-
-  # use the local-exec provisioner to run commands locally
-  /*  provisioner "local-exec" {
-    command = "echo ${aws_instance.test1.public_ip} > ip_address.txt"
-  } */
-}
-
-resource "aws_eip" "ip" {
-  instance = "${aws_instance.test1.id}"
-}
-
-# output variables such as IP address
-output "ip" {
-  value = "${aws_eip.ip.public_ip}"
 }
 
 #server1
@@ -26,11 +12,6 @@ resource "aws_instance" "server1" {
   ami           = "${lookup(var.amis, var.aws_region)}"
   instance_type = "t2.micro"
   subnet_id     = "${var.aws_subnet_id}"
-
-  # use the local-exec provisioner to run commands locally
-  /*  provisioner "local-exec" {
-    command = "echo ${aws_instance.test1.public_ip} > ip_address.txt"
-  } */
 }
 
 #server2
@@ -38,9 +19,4 @@ resource "aws_instance" "server2" {
   ami           = "${lookup(var.amis, var.aws_region)}"
   instance_type = "t2.micro"
   subnet_id     = "${var.aws_subnet_id}"
-
-  # use the local-exec provisioner to run commands locally
-  /*  provisioner "local-exec" {
-    command = "echo ${aws_instance.test1.public_ip} > ip_address.txt"
-  } */
 }
