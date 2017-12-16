f9ae8e485df15ab484605c8a608905bb9fe50fcb
lateef <lateef.koshemani@konga.com>
2017-12-16 12:42:26 +0100

    1. added name tags for the instances 2. added provisioner to install nginx 3. added static IP address

--- instance.tf
+++ instance.tf
@@ -7,6 +7,31 @@ resource "aws_instance" "LB" {
   subnet_id       = "${var.aws_subnet_id}"
   key_name        = "${var.key_name}"
   security_groups = "${var.sec_group}"
+
+  #assign a name tag to instance
+  tags {
+    Name = "LB"
+  }
+
+  #use the remote_exec provisioner to run commands on the remote resource
+  #install nginx
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update && sudo apt-get install nginx -y",
+    ]
+  }
+
+  #configure how the provisioner connects to the resource
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = "${file("~/Documents/personal/lat_aws")}"
+  }
+}
+
+#add a static IP address to the LB instance
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.LB.id}"
 }
 
 #server1
@@ -18,6 +43,24 @@ resource "aws_instance" "server1" {
   security_groups = "${var.sec_group}"
 
   #availability_zone = "eu-west-1a"
+
+  #assign a name tag to instance
+  tags {
+    Name = "server1"
+  }
+  #use the remote_exec provisioner to run commands on the remote resource
+  #install nginx
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update && sudo apt-get install nginx -y",
+    ]
+  }
+  #configure how the provisioner connects to the resource
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = "${file("~/Documents/personal/lat_aws")}"
+  }
 }
 
 #server2
@@ -29,4 +72,22 @@ resource "aws_instance" "server2" {
   security_groups = "${var.sec_group}"
 
   #availability_zone = "eu-west-1b"
+
+  #assign a name tag to instance
+  tags {
+    Name = "server2"
+  }
+  #use the remote_exec provisioner to run commands on the remote resource
+  #install nginx
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update && sudo apt-get install nginx -y",
+    ]
+  }
+  #configure how the provisioner connects to the resource
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = "${file("~/Documents/personal/lat_aws")}"
+  }
 }
