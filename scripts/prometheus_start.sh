f77f2ab7f65096fcf5c54a9260cec01464ba34bd
Lateef Koshemani <drlatt@users.noreply.github.com>
2017-12-17 23:50:07 +0100

    added the urls for server1 and server2 to be monitored by prometheus

--- scripts/prometheus_start.sh
+++ scripts/prometheus_start.sh
@@ -7,7 +7,7 @@ scrape_configs:
   - job_name: "node"
     scrape_interval: "15s"
     target_groups:
-    - targets: ['localhost:9100']
+    - targets: ['localhost:9100','server1.lat.com:9100','server2.lat.com:9100']
 DOH
 
 nohup ./prometheus > /var/log/prometheus.log 2>&1 &
